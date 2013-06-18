70000e2b9224c3909874526b347b21810307ae2b
Elicia <elicia_cor@hotmail.com>
2013-06-18 10:59:29 -0500

    Subiendo archivos estaticos

--- prueba/principal/views.py
+++ prueba/principal/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def inicio(request):
-	return render_to_response('inico.html')
+	return render_to_response('inico.html',context_instance=RequestContext(request))
